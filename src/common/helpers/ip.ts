13f4d2beee78e252e3155f45e908acfd5966c712
Oleksandr Sliusarchuk <o.sliusarchuk@heavensoft.com.ua>
2025-06-05 11:05:08 +0300

    s

--- src/common/helpers/ip.ts
+++ src/common/helpers/ip.ts
@@ -1,3 +1,5 @@
 import { Request } from 'express';
 
-export const getIp = (req: Request) => process.env.NODE_ENV === "development" ? '178.136.111.74' : req.headers['x-forwarded-for']?.toString().split(',')[0]?.trim() || req.socket?.remoteAddress || req.ip;
+export const getIp = (req: Request) => {
+    return process.env.NODE_ENV === "development" ? '178.136.111.74' : req.headers['x-real-ip'] || (req.headers['x-forwarded-for']?.toString().split(',')[0]?.trim() || req.socket?.remoteAddress || req.ip);
+}
